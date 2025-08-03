64df71e2e6e0b83c5add5b34581dfc88a0d82866
Mobashshir <ahsanmobashshir786@gmail.com>
2025-08-03 14:59:13 +0530

    Add proper render instead of return HttpResponse

--- django_internship_project/django_internship_project/views.py
+++ django_internship_project/django_internship_project/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello, This is our First Django Website")
-    return render(request, 'home.html')
+    # return HttpResponse("Hello, This is our First Django Website")
+    return render(request, 'index.html')
 
 def about(request):
     return HttpResponse("Hello, This is our About Page")
